9a637b8b62d923efbfbef4034a192918c033d7c5
Seth Etter <sethetter@gmail.com>
2021-05-19 10:48:44 -0500

    Hooks, ex6, ec1

---
+++ react-hooks/src/exercise/06.extra-1.js
@@ -0,0 +1,68 @@
+// useEffect: HTTP requests
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+// 🐨 you'll want the following additional things from '../pokemon':
+// fetchPokemon: the function we call to get the pokemon info
+// PokemonInfoFallback: the thing we show while we're loading the pokemon info
+// PokemonDataView: the stuff we use to display the pokemon info
+import {
+  PokemonForm,
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonDataView,
+} from '../pokemon'
+
+function PokemonInfo({pokemonName}) {
+  // 🐨 Have state for the pokemon (null)
+  const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
+  const [status, setStatus] = React.useState('idle')
+
+  React.useEffect(() => {
+    ;(async () => {
+      if (!pokemonName || pokemonName === '') return
+      setStatus('pending')
+      try {
+        const pokemonData = await fetchPokemon(pokemonName)
+        setPokemon(pokemonData)
+        setStatus('resolved')
+      } catch (e) {
+        setError(e)
+        setStatus('rejected')
+      }
+    })()
+  }, [pokemonName])
+
+  if (status === 'idle') return 'Submit a pokemon'
+  if (status === 'pending') return <PokemonInfoFallback name={pokemonName} />
+  if (status === 'rejected')
+    return (
+      <div role="alert">
+        There was an error:
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
+  if (status === 'resolved') return <PokemonDataView pokemon={pokemon} />
+  throw new Error('unreachable')
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName)
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className="pokemon-info">
+        <PokemonInfo pokemonName={pokemonName} />
+      </div>
+    </div>
+  )
+}
+
+export default App
